5cc1a4d639204051c8a2b7e9d34d04e8b35edf8c
Felip7Dz <154281713+Felip7Dz@users.noreply.github.com>
2024-01-13 13:04:17 +0100

    Conexion con BBDD funcionando

--- src/main/java/com/app/config/SecurityConfig.java
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -1,26 +1,45 @@
 package com.app.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.app.login.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private DataSource dataSource;
 
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
+	}	
+	
+	/*
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
@@ -32,6 +51,7 @@ public class SecurityConfig {
 			).csrf(AbstractHttpConfigurer::disable)
 			.formLogin(formLogin ->
 		    formLogin
+		    	.usernameParameter("user_login")
 		        .loginPage("/login")
 		        .permitAll()
 		        .defaultSuccessUrl("/home", true)
@@ -39,35 +59,5 @@ public class SecurityConfig {
 
 		return http.build();
 	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(
-			UserDetailsService userDetailsService,
-			PasswordEncoder passwordEncoder) {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(userDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-		ProviderManager providerManager = new ProviderManager(authenticationProvider);
-		providerManager.setEraseCredentialsAfterAuthentication(false);
-
-		return providerManager;
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-			.username("admin")
-			.password("admin")
-			.roles("ADMIN")
-			.build();
-
-		return new InMemoryUserDetailsManager(userDetails);
-	}
- 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	}
-
+	*/
 }
--- src/main/java/com/app/controller/LoginController.java
+++ src/main/java/com/app/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.app.controller;
 
 import java.net.ConnectException;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +14,31 @@ import com.app.service.LoginService;
 @Controller
 public class LoginController {
 	
+	  @Autowired
+	
 	private final LoginService loginService;
 	
 	public LoginController(LoginService loginService) {
 		this.loginService = loginService;
 	}
+
 	
 	@GetMapping("/login")
 	public String login(Model model) {
 		return "public/login";
 	}
 	
+	@GetMapping("/logout")
+    public String logout() {
+        return "redirect:/login";
+    }
+	
 	@PostMapping("/register")
 	public String register(UserDTO user, Model model) {
-		 BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		try {
 			String tmpCheck = loginService.checkUserInDB(user.getUsuario());
 			if("1".equals(tmpCheck)) {
-				String tmpPass = passwordEncoder.encode(user.getPassw());
+				String tmpPass = user.getPassw();
 				user.setPassw(tmpPass);
 				user.setRole("USER");
 				loginService.createUserInDB(user);
