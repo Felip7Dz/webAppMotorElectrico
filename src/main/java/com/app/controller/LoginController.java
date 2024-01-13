64aa490553a41b0ac7d6ddcf45f27b5f250309d7
Felip7Dz <154281713+Felip7Dz@users.noreply.github.com>
2024-01-13 17:58:37 +0100

    Passw Encoder en Registro

--- src/main/java/com/app/controller/LoginController.java
+++ src/main/java/com/app/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.app.controller;
 import java.net.ConnectException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,43 +14,44 @@ import com.app.service.LoginService;
 
 @Controller
 public class LoginController {
-	
-	  @Autowired
-	
+
+	@Autowired
 	private final LoginService loginService;
 	
-	public LoginController(LoginService loginService) {
+	BCryptPasswordEncoder passwEncoder;
+
+	public LoginController(LoginService loginService, BCryptPasswordEncoder passwEncoder) {
 		this.loginService = loginService;
+		this.passwEncoder = passwEncoder;
 	}
 
-	
 	@GetMapping("/login")
 	public String login(Model model) {
 		return "public/login";
 	}
-	
+
 	@GetMapping("/logout")
-    public String logout() {
-        return "redirect:/login";
-    }
-	
+	public String logout() {
+		return "redirect:/login";
+	}
+
 	@PostMapping("/register")
 	public String register(UserDTO user, Model model) {
 		try {
 			String tmpCheck = loginService.checkUserInDB(user.getUsuario());
-			if("1".equals(tmpCheck)) {
-				String tmpPass = user.getPassw();
+			if ("1".equals(tmpCheck)) {
+				String tmpPass = passwEncoder.encode(user.getPassw());
 				user.setPassw(tmpPass);
 				user.setRole("USER");
 				loginService.createUserInDB(user);
 				model.addAttribute("userCreated", "User Created Successfully");
-			}else {
+			} else {
 				model.addAttribute("userAlreadyExists", "User Already Exists");
 			}
 		} catch (ConnectException e) {
 			System.err.println("Error al conectar con la API: " + e.getMessage());
 		}
-		
+
 		return "public/login";
 	}
 }
