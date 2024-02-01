8fcdd31910958b8dd8c0a8311360b84c79f9d491
Felip7Dz <154281713+Felip7Dz@users.noreply.github.com>
2024-02-01 13:30:37 +0100

    Correccion fallo constantes y login

--- src/main/java/com/app/constants/ViewConstants.java
+++ src/main/java/com/app/constants/ViewConstants.java
@@ -10,13 +10,13 @@ public class ViewConstants {
 	public static final String VIEW_LOGIN_PAGE = "public/login";
 	
 	/** REDIRECT_LOGIN_PAGE */
-	public static final String REDIRECT_LOGIN_PAGE = "redirect:/login";
+	public static final String REDIRECT_LOGIN_PAGE = "redirect:/webAppMotorElectrico/login";
 	
 	/** VIEW_HOME_PAGE */
 	public static final String VIEW_HOME_PAGE = "public/home";
 	
 	/** REDIRECT_HOME_PAGE */
-	public static final String REDIRECT_HOME_PAGE = "redirect:/home";
+	public static final String REDIRECT_HOME_PAGE = "redirect:/webAppMotorElectrico/home";
 	
 	/** VIEW_PRELOADED_PAGE */
 	public static final String VIEW_PRELOADED_PAGE = "public/preloaded";
@@ -25,5 +25,5 @@ public class ViewConstants {
 	public static final String VIEW_NEWLOADED_PAGE = "public/newLoad";
 	
 	/** REDIRECT_NEWLOADED_PAGE */
-	public static final String REDIRECT_NEWLOADED_PAGE = "redirect:/newload?selectedSavedModel=";
+	public static final String REDIRECT_NEWLOADED_PAGE = "redirect:/webAppMotorElectrico/newload?selectedSavedModel=";
 }
